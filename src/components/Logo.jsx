8c5ce5852552c4cfaba885e06d752bf33ec5825f
Ivan <i.popovych.work@gmail.com>
2025-09-18 12:15:43 +0300

    misc

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -6,7 +6,7 @@ const Logo = () => {
   return (
     <a
       href=""
-      className="flex items-center gap-2 bg-transparent md:bg-[#EFEFF2] md:border md:border-[#31343912] rounded-full py-2 px-3"
+      className="flex items-center gap-2 bg-transparent xl:bg-[#EFEFF2] xl:border xl:border-[#31343912] rounded-full py-2 xl:px-3"
     >
       <div className="w-5 h-5  md:w-6 2xl:h-6">
         <img className="w-full h-full object-contain" src={logo} alt="logo" />
